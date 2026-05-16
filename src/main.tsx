b6d2809f6d1754a668a7834a7d8dfe9975436348
ginko-friday <zhurenbao@zhurenbaodeMacBook-Pro.local>
2026-05-17 03:28:53 +0800

    feat: 接入 Clerk Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/react'
 import { ThemeProvider } from './hooks/useTheme'
 import App from './App'
 import './index.css'
 
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <ClerkProvider publishableKey={publishableKey} afterSignInUrl="/">
+      <BrowserRouter>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>,
 )
